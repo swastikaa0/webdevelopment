613b4c177be69d7158c07c34e379cf2c843e26d8
swastikaa0 <swastikadhungana2004@gmail.com>
2025-04-19 17:08:34 +0545

    second commit

--- app/src/main/java/com/example/webdevelopment/arraylist.kt
+++ app/src/main/java/com/example/webdevelopment/arraylist.kt
@@ -6,8 +6,17 @@ fun main(args: Array<String>) {
     age1.add(1, 20)  // Insert 20 at index 1
     age1.add(5)
 
-    // We can add value directly in array while initializing variables.
+    println("Elements in age1:")
+    for (age in age1) {
+        println(age)
+    }
+    println()
+
     var age2 = arrayListOf<Int>(1, 20, 5)
+    println("Initial size of age2: ${age2.size}")
+    age2.clear()
+    println("Size of age2 after clearing: ${age2.size}")
+    println()
 
     var name = arrayListOf<String>("sandis", "ram", "shyam")
     name.add("hari")
@@ -16,12 +25,21 @@ fun main(args: Array<String>) {
     name.remove("shyam")
     name.removeAt(0)
 
-    println(name)
+    println("Current names: $name")
+
+    val searchName = "hari"
+    if (name.contains(searchName)) {
+        println("The name '$searchName' exists in the list.")
+    } else {
+        println("The name '$searchName' does not exist in the list.")
+    }
+    println()
 
     var mixArrayList = arrayListOf<Any>("hello", 5, 2.0)
-    println(mixArrayList[0])
-    println(mixArrayList[1])
-    println(mixArrayList[2])
+    println("Size of mixArrayList: ${mixArrayList.size}")
+    println("Element at index 0 of mixArrayList: ${mixArrayList[0]}")
+    println("Element at index 1 of mixArrayList: ${mixArrayList[1]}")
+    println("Element at index 2 of mixArrayList: ${mixArrayList[2]}")
 }
 
 
